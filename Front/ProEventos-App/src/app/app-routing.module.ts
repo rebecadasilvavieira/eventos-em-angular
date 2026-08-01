b2eeeb67d8d421799cc77905aa3f6a07fa2722b3
Rebeca <rebecadasilva.v3@gmail.com>
2026-08-01 20:52:39 -0300

    feat: adiciona formulários/mudança de layout

--- Front/ProEventos-App/src/app/app-routing.module.ts
+++ Front/ProEventos-App/src/app/app-routing.module.ts
@@ -4,15 +4,33 @@ import { Routes, RouterModule } from '@angular/router';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
 import { ContatosComponent } from './components/contatos/contatos.component';
+import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
+import { UserComponent } from './components/user/user.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
+import { LoginComponent } from './components/user/login/login.component';
 
-const routes: Routes = [
-  { path: 'eventos', component: EventosComponent },
+const routes: Routes = [ {
+  path: 'user', component : UserComponent, children: [
+    { path: 'login', component: LoginComponent },
+    { path: 'registration', component: RegistrationComponent }
+  ]
+},
+{ path: 'user/perfil', component: PerfilComponent },
+
+  { path: 'eventos', redirectTo: 'eventos/lista', pathMatch: 'full' },
+  {  path: 'eventos', component: EventosComponent, children: [
+    { path: 'detalhe/:id', component: EventoDetalheComponent },
+    { path: 'detalhe', component: EventoDetalheComponent },
+    { path: 'lista', component: EventoListaComponent },
+  ],
+  },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'palestrantes', component: PalestrantesComponent },
   { path: 'contatos', component: ContatosComponent },
-  { path: 'perfil', component: PerfilComponent },
+
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: 'dashboard', pathMatch: 'full' },
 ];
