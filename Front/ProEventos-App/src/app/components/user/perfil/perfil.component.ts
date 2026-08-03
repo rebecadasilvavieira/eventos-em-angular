9ebca01fc2f23f91c7140a553ec3d9d286891519
Rebeca <rebecadasilva.v3@gmail.com>
2026-08-03 10:36:05 -0300

    feat: adiciona validação nos formulários de perfil registration e mais

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(public fb: FormBuilder) { }
 
-  ngOnInit() {
+
+
+ ngOnInit(): void {
+  this.validation();
+}
+
+private validation(): void {
+
+  const formOptions: AbstractControlOptions = {
+    validators: ValidatorField.MustMatch('senha', 'confirmeSenha')
+  };
+
+  this.form = this.fb.group({
+    primeiroNome: ['', Validators.required],
+    ultimoNome: ['', Validators.required],
+    email: ['', [
+      Validators.required,
+      Validators.email
+    ]],
+    userName: ['', Validators.required],
+    senha: ['', [
+      Validators.required,
+      Validators.minLength(6)
+    ]],
+    confirmeSenha: ['', Validators.required],
+  }, formOptions);
+}
+
+
+  get f(): any { return this.form.controls; }
+  onSubmit(): void {
+    if (this.form.invalid) {
+      return;
+    }
   }
 
-}
+
+public resetForm(event: any): void {
+  event.preventDefault();
+  this.form.reset();
+}}
+
